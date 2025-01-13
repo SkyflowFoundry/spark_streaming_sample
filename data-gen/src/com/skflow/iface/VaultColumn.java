5ed79d4399adbeb9044e28c252354ff8104e9f6f
Manish Ahluwalia <manish.ahluwalia@gmail.com>
2025-01-13 10:03:05 -0700

    Restrict annotations to right types

--- data-gen/src/com/skflow/iface/VaultColumn.java
+++ data-gen/src/com/skflow/iface/VaultColumn.java
@@ -1,9 +1,12 @@
 package com.skflow.iface;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface VaultColumn {
     /**
      * The name of the column.

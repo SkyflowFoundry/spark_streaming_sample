cffcd7df7955baafc531f499b059019b2b8f77dc
Manish Ahluwalia <manish.ahluwalia@gmail.com>
2025-02-05 15:22:19 -0700

    Remove unneeded config items

--- data-gen/src/com/skyflow/walmartpoc/Config.java
+++ data-gen/src/com/skyflow/walmartpoc/Config.java
@@ -8,17 +8,12 @@ import org.yaml.snakeyaml.Yaml;
 
 public class Config {
     public static class SeedData {
-        public int total_customer_count;
-        public int total_catalog_size;
         public String output_directory;
         public String customers_file;
         public String payments_file;
         public String catalog_file;
         public String transactions_file;
         public String consent_file;
-        public boolean load_to_vault;
-        public String tokenized_customers_file;
-        public String tokenized_payments_file;
     }
     public static class FakeData {
         // How many cards does a customer have? Probabilities that must add to <=1. Diff from 1 is 3 cards prob
@@ -34,19 +29,8 @@ public class Config {
         public float insert_customer_probability;
         public float add_payment_probability;
     }
-    public static class VaultConfig {
-        public String mgmt_url;
-        public String account_id;
-        public String workspace_id;
-        public String vault_id;
-        public String vault_url;
-        public String private_key_file;
-        public int max_rows_in_batch;
-
-    }
     public FakeData fake_data;
     public SeedData seed_data;
-    public VaultConfig vault;
     public UpsertProbabilities upsert_probabilities;
 
 
